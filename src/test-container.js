6495745e1b36d0e5b73ccd8cd65de254c2957ad4
Fenzland <Fenzland@github.com>
2019-11-10 07:50:24 +0800

    test-container::addTest() support accept named function and set its name as test name

--- src/test-container.js
+++ src/test-container.js
@@ -14,5 +14,8 @@ export async function requestContainer()
 
 export function addTest( name, test, )
 {
+	if( typeof name === 'function' )
+		return addTest( name.name, name, );
+	
 	(container|| rootContainer).push( { name, test, }, );
 }
