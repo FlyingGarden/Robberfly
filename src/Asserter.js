069519940ddec96c51229f0695f4a8e34968017b
Fenzland <Fenzland@github.com>
2019-11-10 07:15:02 +0800

    Asserter hold a assertion counter and failures container, get them with static method to prevent over providing

--- src/Asserter.js
+++ src/Asserter.js
@@ -1,5 +1,17 @@
 
 export default class Asserter
 {
+	#counter= 0;
 	
+	#failures= [];
+	
+	#pushFailure= failure=> this.#failures.push( Object.freeze( failure, ), );
+	
+	static getResult( asserter, )
+	{
+		return {
+			assertions: asserter.#counter,
+			failures: [ ...asserter.#failures, ],
+		};
+	}
 }
