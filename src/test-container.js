94bcc111a76e3c9570a91a73f5917da480d91fb2
Fenzland <Fenzland@github.com>
2019-11-10 07:58:20 +0800

    test-conainer::reqquestContainer() request container to add tests serially

--- src/test-container.js
+++ src/test-container.js
@@ -1,4 +1,5 @@
 
+let holding= undefined;
 let container= undefined;
 const rootContainer= [];
 
@@ -9,7 +10,29 @@ export async function collectTests( callback, )
 
 export async function requestContainer()
 {
+	while( holding )
+		await holding;
 	
+	let free;
+	holding= new Promise( resolve=> {
+		free= resolve;
+	}, );
+	
+	container= [];
+	
+	return {
+		unload(){
+			
+			const tests= container;
+			
+			holding= undefined;
+			container= undefined;
+			
+			free();
+			
+			return tests;
+		},
+	};
 }
 
 export function addTest( name, test, )
