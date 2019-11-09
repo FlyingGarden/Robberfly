d6c30c12f6ef833db4652d142c25e204a5f84f69
Fenzland <Fenzland@github.com>
2019-11-10 07:25:41 +0800

    add util isPureObject()

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,16 @@ export function areOneThing( x, y, )
 {
 	return new Set( [ x, y, ], ).size === 1;
 }
+
+export function isPureObject( x, )
+{
+	return (
+		x
+	&&
+		typeof x === 'object'
+	&&
+		x.constructor === Object
+	&&
+		Reflect.getPrototypeOf( x, ) === Object.prototype
+	);
+}
