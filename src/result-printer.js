8613b28de0937dc44571360d7e6decbd268c1472
Fenzland <Fenzland@github.com>
2019-11-10 08:05:46 +0800

    result-printer print result with different printer at different environment

--- src/result-printer.js
+++ src/result-printer.js
@@ -4,10 +4,25 @@ const printerEnv= env|| 'console';
 
 export async function printResult( result, )
 {
-	
+	return print( 'Result', result, );
 }
 
 export async function printResults( results, )
 {
+	if( results.length === 1 )
+		return printResult( results[0], );
 	
+	return print( 'Results', results, );
+}
+
+async function print( target, data, )
+{
+	const [ Renderer, Printer, ]= await Promise.all( [
+		import (`./${printerEnv}/renderer.js`),
+		import (`./${printerEnv}/printer.js`),
+	], );
+	
+	const output= await Renderer[`render${target}`]( data, );
+	
+	return Printer.print( output, );
 }
