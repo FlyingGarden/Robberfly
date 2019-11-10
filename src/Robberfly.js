2241c7af9fb1d0062299c4babf9ea6112bc9f141
Fenzland <Fenzland@github.com>
2019-11-10 08:35:54 +0800

    add Robberfy::#resolvePath() with path module

--- src/Robberfly.js
+++ src/Robberfly.js
@@ -1,5 +1,18 @@
+import * as Path from '../modules/path.js';
 
 export default class Robberfly
 {
 	
+}
+
+/**
+ * @param path (string)
+ * 
+ * @return (string)
+ */
+function resolvePath( path, )
+{
+	const basePath= Path.traceBack( 3, ).replace( /\/[^\/]*$/, '', );
+	
+	return Path.resolve( basePath, path, );
 }
